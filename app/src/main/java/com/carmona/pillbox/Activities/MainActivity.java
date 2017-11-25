74645f84991bf057d304607bade517dfccfd32d1
Carmona <13030617@itcelaya.edu.mx>
2017-11-24 22:46:15 -0600

    Se agrega alert dialog de contacto

--- app/src/main/java/com/carmona/pillbox/Activities/MainActivity.java
+++ app/src/main/java/com/carmona/pillbox/Activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.carmona.pillbox.Classes.About;
 import com.carmona.pillbox.R;
 
 public class MainActivity extends AppCompatActivity
@@ -91,7 +92,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_notificacion) {
 
         } else if (id == R.id.nav_contacto) {
-
+            About a = new About(this);
+            a.alerta();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/carmona/pillbox/Classes/About.java
+++ app/src/main/java/com/carmona/pillbox/Classes/About.java
@@ -1,9 +1,13 @@
 package com.carmona.pillbox.Classes;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.webkit.WebView;
 
@@ -14,40 +18,28 @@ import com.carmona.pillbox.R;
  */
 
 public class About {
-
+    String mensaje="Pillbox ver Early Access \n Developed By Carmona at Bandido Studios \n Powered by Alio Mexico.";
+    String titulo= "Pillbox";
     Context context;
-    private About(Context c){
+    public About(Context c){
         context=c;
     }
+    public void alerta(){
+    AlertDialog.Builder alert = new AlertDialog.Builder(context);
+    alert.setTitle(titulo);
+    alert.setMessage(mensaje);
+    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int whichButton) {
 
-    private void aboutDialog() {
-        AlertDialog.Builder alert = new AlertDialog.Builder(context);
-        alert.setTitle("Forecastie");
-        final WebView webView = new WebView(context);
-        String about = "<p>1.5</p>" +
-                "<p>A lightweight, opensource weather app.</p>" +
-                "<p>Developed by <a href='mailto:t.martykan@gmail.com'>Tomas Martykan</a></p>" +
-                "<p>Data provided by <a href='http://openweathermap.org/'>OpenWeatherMap</a>, under the <a href='http://creativecommons.org/licenses/by-sa/2.0/'>Creative Commons license</a>" +
-                "<p>Icons are <a href='https://erikflowers.github.io/weather-icons/'>Weather Icons</a>, by <a href='http://www.twitter.com/artill'>Lukas Bischoff</a> and <a href='http://www.twitter.com/Erik_UX'>Erik Flowers</a>, under the <a href='http://scripts.sil.org/OFL'>SIL OFL 1.1</a> licence.";
-        //TypedArray ta = obtainStyledAttributes(new int[]{android.R.attr.textColorPrimary, R.attr.colorAccent});
-        String textColor = String.format("#%06X", (0xFFFFFF & ta.getColor(0, Color.BLACK)));
-        String accentColor = String.format("#%06X", (0xFFFFFF & ta.getColor(1, Color.BLUE)));
-        //ta.recycle();
-        about = "<style media=\"screen\" type=\"text/css\">" +
-                "body {\n" +
-                "    color:" + textColor + ";\n" +
-                "}\n" +
-                "a:link {color:" + accentColor + "}\n" +
-                "</style>" +
-                about;
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        webView.loadData(about, "text/html", "UTF-8");
-        alert.setView(webView, 32, 0, 32, 0);
-        alert.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
+        }
+    });
+    /*
+    alert.setNegativeButton("Cancel",
+            new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int whichButton) {
+        }
+    });*/
 
-            }
-        });
-        alert.show();
+    alert.show();
     }
 }
